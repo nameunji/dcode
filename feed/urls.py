31d0223782653a3bec47aee9429638e65b6d32b0
nameunji <namji117@gmail.com>
2020-03-18 20:08:29 +0900

    feature/패션피드상세

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import FeedListView
+from .views      import FeedListView, FeedDetailView
 
 urlpatterns = [
-    path('', FeedListView.as_view())
+    path('', FeedListView.as_view()),
+    path('/<int:feed_id>', FeedDetailView.as_view()),
 ]
--- feed/views.py
+++ feed/views.py
@@ -33,4 +33,21 @@ class FeedListView(View):
 
         return JsonResponse({'list': data, 'totalCount':total_count}, status = 200)
 
-
+class FeedDetailView(View):
+    def get(self, request, feed_id):
+        try:
+            feed = Feed.objects.prefetch_related('feedthumbnail_set').get(id = feed_id)
+            data = {
+                'id'            : feed.id,
+                'tags'          : [ el['tag'] for el in feed.tag.values() ],
+                'mdThumb'       : [ el['url'] for el in feed.feedthumbnail_set.values() ],
+                'title'         : feed.title,
+                'content'       : feed.content,
+                'updated_at'    : feed.updated_at.strftime("%Y-%m-%d %H:%M"),
+                'count_like'    : feed.count_like,
+                'count_comment' : feed.count_comment,
+                'count_shared'  : feed.count_shared
+            }
+            return JsonResponse({'data': data}, status = 200)
+        except Feed.DoesNotExist:
+            return JsonResponse({'message' : "DOES_NOT_EXIST_FEED"}, status = 400)
